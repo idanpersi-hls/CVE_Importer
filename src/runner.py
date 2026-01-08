f4e7f9060f6978893b8877e68d5962af06984603
idanpersi-hls <idan.persi@moveohls.com>
2026-01-08 19:57:47 +0200

    add smoke test conf for runner

--- src/runner.py
+++ src/runner.py
@@ -4,6 +4,7 @@ from src.nvd_client import fetch_cves
 from src.normalize import format_cve
 from src.sql_db import upsert_cves, init_db, close_connection_pool
 from src.config import RESULTS_PER_PAGE, START_DATE
+import os, sys
 
 def load_to_db(vuln_list: List[Dict]) -> None:
     normalized = {}
@@ -41,7 +42,11 @@ def load_by_dates(pubStartDate: str) -> str:
 def run(start_date: str | None = None) -> None:
     if not start_date:
         raise ValueError("START_DATE must be set in configuration")
-    init_db()
+    if os.getenv("SMOKE_TEST")=="true":
+        print("smoke test succesfull")
+        sys.exit(0)
+    else:
+        init_db()
     try:
         num_cycles = cycles_to_wanted_date(start_date, now_utc_iso())
         current_start = start_date
